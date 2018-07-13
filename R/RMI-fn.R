1e6f090c1c584804f41a5cca9b3be061aebb9df6
alemermartinez <38252440+alemermartinez@users.noreply.github.com>
2018-07-13 12:43:53 -0300

    Bandwidth warning fixed

--- R/RMI-fn.R
+++ R/RMI-fn.R
@@ -116,8 +116,8 @@ margint.cl <- function(Xp, yp, point=NULL, windows, epsilon, prob=NULL,
     if(is.null(degree)){
       stop("Degree of local polynomial missing")
     }else{
-      if( is.null(dim(windows)) ){
-        stop("Windows should be a vector o a matrix")
+      if( is.null(dim(windows)) | (!is.vector(windows)) ){
+        stop("Windows should be a vector or a matrix")
       }
     }
   }else{
@@ -439,7 +439,7 @@ margint.rob <- function(Xp, yp, point=NULL, windows, prob=NULL, sigma.hat=NULL,
     if(is.null(degree)){
       stop("Degree of local polynomial missing")
     }else{
-      if( is.null(dim(windows)) ){
+      if( is.null(dim(windows)) | (!is.vector(windows)) ){
         stop("Windows should be a vector o a matrix")
       }
     }
